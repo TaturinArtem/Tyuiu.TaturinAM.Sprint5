dfd8ad660d4b6ad4c15e861e958ecda75056f1fc
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-11-16 14:57:31 +0500

    Task0
    Task0

---
+++ Tyuiu.TaturinAM.Sprint5.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.TaturinAM.Sprint5.Task0.V13.Lib;
+namespace Tyuiu.TaturinAM.Sprint5.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            string path = @"C:\Users\Tatur\source\repos\Tyuiu.TaturinAM.Sprint5\Tyuiu.TaturinAM.Sprint5.Task0.V13\bin\Debug\OutOutFileTask0.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, true);
+        }
+    }
+}
