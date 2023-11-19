2531affd4caafb074ff4f745712a117d11130ced
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-11-19 15:15:43 +0500

    Task3
    Task3

---
+++ Tyuiu.TaturinAM.Sprint5.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TaturinAM.Sprint5.Task3.V6.Lib;
+using System.IO;
+namespace Tyuiu.TaturinAM.Sprint5.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Tatur\source\repos\Tyuiu.TaturinAM.Sprint5\Tyuiu.TaturinAM.Sprint5.Task3.V6\bin\Debug\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.TaturinAM.Sprint5.Task3.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TaturinAM.Sprint5.Task3.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TaturinAM.Sprint5.Task3.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df081940-739c-49b8-bba8-3166417f1ada")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
